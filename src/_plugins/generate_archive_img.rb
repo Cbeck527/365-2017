81b466e681614933993bd17b02d434823bf649c8
Chris Becker <cbeck527@gmail.com>
2016-08-03 01:22:41 -0400

    Add archive page, refactor homepage
    * Add archive page
    * Refactor homepage to show images. Previously, this was the idea for
      the archive page, but it makes sense to show thumbnail images right
      away rather than after a few clicks
    * Write (very) basic jekyll plugin to copy full-res images to a
      thumbnail directory and resize for quick page loads.

---
+++ src/_plugins/generate_archive_img.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+
+Jekyll::Hooks.register :posts, :post_write do |post|
+  puts "Generating thumbnail size image for post " + post.data['title']
+  FileUtils.mkdir_p "_site/images/thumb"
+  FileUtils.cd("src/images/") do
+    FileUtils.cp post.data['image'], "../../_site/images/thumb/"
+  end
+  FileUtils.cd("_site/images/thumb/") do
+    system('mogrify -resize "600x600>" ' + post.data["image"])
+  end
+end
